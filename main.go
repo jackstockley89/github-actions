84ed9f5dec937920c79d9ed6b1e5495f99c6f35a
Jack Stockley <jack.stockley@digital.justice.gov.uk>
2021-09-03 16:53:52 +0100

    Adding functions
    Moving api calls into functiions

--- main.go
+++ main.go
@@ -10,9 +10,19 @@ import (
 	"golang.org/x/oauth2"
 )
 
-//get repo owner
-func repoOwner() {
+type Collaborators struct {
+	User []string
+}
 
+type PullRequests struct {
+	Title []string
+	User  []string
+}
+
+// get repo owner
+func getRepoOwner() {
+	// get rnv token
+	// Connect to giithub
 	token := os.Getenv("GITHUB_OAUTH_TOKEN")
 
 	ctx := context.Background()
@@ -29,13 +39,16 @@ func repoOwner() {
 		log.Fatalln(err)
 	}
 	for _, collab := range repos {
-		fmt.Println("Collaborator:", *collab.Login)
+		collabUserslice := Collaborators{
+			User: []string{*collab.Login}}
+		fmt.Println("Collaborator:", collabUserslice)
 	}
 }
 
-//get pr owner
-func prOwner() {
-
+// get pr owner
+func getPrOwner() {
+	// get rnv token
+	// Connect to giithub
 	token := os.Getenv("GITHUB_OAUTH_TOKEN")
 
 	ctx := context.Background()
@@ -45,22 +58,37 @@ func prOwner() {
 	tc := oauth2.NewClient(ctx, ts)
 
 	client := github.NewClient(tc)
-
 	// list public pull request for "golangwebpage" repo
 	prs, _, err := client.PullRequests.List(context.Background(), "jackstockley89", "golangwebpage", nil)
 	if err != nil {
 		log.Fatalln(err)
 	}
 	for _, pr := range prs {
-		fmt.Println("Pull Request Title:", *pr.Title, "\nPull Request User:", *pr.User.Login)
+		prTitleslice := PullRequests{
+			Title: []string{*pr.Title}}
+		prUserslice := PullRequests{
+			User: []string{*pr.User.Login}}
+		fmt.Println("\nPull Request Title:", prTitleslice, "\nPull Request User:", prUserslice)
+	}
+
+}
+
+// compare pr and repo owner
+func compareCheck() {
+	resp1 := "jackstockley89" // This should come from the getRepoOwner
+	resp2 := "jackstockley89" // This should come from the getPrOwner
+	// pull in data from struct for both collba and pr
+	if resp1 == resp2 {
+		fmt.Println("Match")
+	} else {
+		fmt.Println("No Match")
 	}
 }
 
 func main() {
-	repoOwner()
-	prOwner()
-	//compare pr and repo owner
-
-	//if pr and repo owner match check passes
-	//if fails delete pr
+	getRepoOwner()
+	getPrOwner()
+	compareCheck()
+	// if pr and repo owner match check passes
+	// if fails delete pr
 }
