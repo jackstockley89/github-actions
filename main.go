1790fc1e25438ee1d2054922d4c515e1f996cd0e
Jack Stockley <jack.stockley@digital.justice.gov.uk>
2021-09-08 16:49:34 +0100

    Adding Compare function
    Adding function that will compare PR and Repo owner

--- main.go
+++ main.go
@@ -5,25 +5,24 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"reflect"
+	"strconv"
+	"strings"
 
 	"github.com/google/go-github/v38/github"
 	"golang.org/x/oauth2"
 )
 
-type Collaborators struct {
-	User []string
-}
-
-type PullRequests struct {
-	Title []string
-	User  []string
-}
-
 // get repo owner
-func getRepoOwner() {
+func gatherInput() {
+	var c []string
 	// get rnv token
 	// Connect to giithub
 	token := os.Getenv("GITHUB_OAUTH_TOKEN")
+	githubref := os.Getenv("GITHUB_REF")
+	sgr := strings.Split(githubref, "/")
+	branch := sgr[2]
+	bid, _ := strconv.Atoi(branch)
 
 	ctx := context.Background()
 	ts := oauth2.StaticTokenSource(
@@ -39,56 +38,34 @@ func getRepoOwner() {
 		log.Fatalln(err)
 	}
 	for _, collab := range repos {
-		collabUserslice := Collaborators{
-			User: []string{*collab.Login}}
-		fmt.Println("Collaborator:", collabUserslice)
+		fmt.Println(*collab.Login)
+		c = []string{*collab.Login}
 	}
-}
 
-// get pr owner
-func getPrOwner() {
-	// get rnv token
-	// Connect to giithub
-	token := os.Getenv("GITHUB_OAUTH_TOKEN")
-
-	ctx := context.Background()
-	ts := oauth2.StaticTokenSource(
-		&oauth2.Token{AccessToken: token},
-	)
-	tc := oauth2.NewClient(ctx, ts)
-
-	client := github.NewClient(tc)
+	// get pr owner
 	// list public pull request for "golangwebpage" repo
-	prs, _, err := client.PullRequests.List(context.Background(), "jackstockley89", "golangwebpage", nil)
+	prs, _, err := client.PullRequests.Get(context.Background(), "jackstockley89", "golangwebpage", bid)
 	if err != nil {
 		log.Fatalln(err)
 	}
-	for _, pr := range prs {
-		prTitleslice := PullRequests{
-			Title: []string{*pr.Title}}
-		prUserslice := PullRequests{
-			User: []string{*pr.User.Login}}
-		fmt.Println("\nPull Request Title:", prTitleslice, "\nPull Request User:", prUserslice)
-	}
+	p := []string{*prs.User.Login}
+	fmt.Println("\n", compareCheck([]string{fmt.Sprintln(c)}, []string{fmt.Sprintln(p)}))
 
 }
 
 // compare pr and repo owner
-func compareCheck() {
-	resp1 := "jackstockley89" // This should come from the getRepoOwner
-	resp2 := "jackstockley89" // This should come from the getPrOwner
-	// pull in data from struct for both collba and pr
-	if resp1 == resp2 {
-		fmt.Println("Match")
-	} else {
-		fmt.Println("No Match")
-	}
+func compareCheck(a1 []string, a2 []string) bool {
+	// a1 // This should come from the client.Repositories.ListCollaborators //"jackstockley89, jasonBirchall"
+	// a2 // This should come from the client.PullRequests.Get //"jackstockley89" or "jasonBirchall"
+	fmt.Println("variable: a1")
+	fmt.Println(a1)
+	fmt.Println("variable: a2")
+	fmt.Println(a2)
+	return reflect.DeepEqual(a1, a2)
 }
 
 func main() {
-	getRepoOwner()
-	getPrOwner()
-	compareCheck()
+	gatherInput()
 	// if pr and repo owner match check passes
 	// if fails delete pr
 }
