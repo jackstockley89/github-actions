facc729ef27b7bc54e280fb6d722295d358e94ce
Jack Stockley <jack.stockley@digital.justice.gov.uk>
2023-03-07 20:37:39 +0000

    fix(secret): Edit secret string to pass token

--- close-pr/close-pr.go
+++ close-pr/close-pr.go
@@ -15,7 +15,7 @@ import (
 func PullRequestClose() {
 	flag.Parse()
 
-	token := flag.String("token", os.Getenv("GITHUB_OAUTH_TOKEN"), "GihHub Personel token string")
+	token := flag.String("token", os.Getenv("REPO_TOKEN"), "GihHub Personel token string")
 	githubrepo := flag.String("githubrepo", os.Getenv("GITHUB_REPOSITORY"), "Github Repository string")
 	githubref := flag.String("githubref", os.Getenv("GITHUB_REF"), "Github Respository PR ref string")
 	c := client.ClientConnect(*token)
